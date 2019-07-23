aacae52a35e6498eb90f42c664bee57573eb5b87
Nicola <nicola.onofri@gmail.com>
2019-07-23 12:47:35 +0200

    Python code init

---
+++ src/utils.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# _*_ coding: utf-8 _*_
+"""Module docstring here"""
+__author__ = "Nicola Onofri, Luigi Bonassi"
+__license__ = "GPL"
+__email__ = "nicola.onofri@gmail.com, l.bonassi005@studenti.unibs.it"
