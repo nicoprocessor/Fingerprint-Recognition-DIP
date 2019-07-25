846f55a73b7cb99b6744e5822349b362cf3b0b58
Nicola <nicola.onofri@gmail.com>
2019-07-25 15:48:55 +0200

    extract_roi -> roi_extraction

--- src/fingerprint_recognition.py
+++ src/fingerprint_recognition.py
@@ -17,26 +17,6 @@ from utils import print_images_args
 from skimage.morphology import skeletonize
 
 
-def extract_roi(img: np.ndarray) -> np.ndarray:
-    """
-    Extracts the Region Of Interest from the original image.
-    The ROI extraction process consists in subtracting the closed image from the opened image
-    and then discarding the borders
-    :param img: the original image
-    :return: the region of interest of the image
-    """
-    # Alternatives: cv2.MORPH_CROSS, cv2.MORPH_ELLIPSE
-    kernel_size = 5
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (kernel_size, kernel_size))
-    closing = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
-    opening = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
-    roi = opening-closing
-
-    # TODO borders removal
-
-    return img
-
-
 def fft_enhancement(img: np.ndarray) -> np.ndarray:
     """
     Image enhancement using fft
