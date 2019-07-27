a77593628ee22930c110574ca0c5183acc5e7d93
Nicola <nicola.onofri@gmail.com>
2019-07-27 16:27:46 +0200

    Precise ridge orientation & method documentation

--- src/fingerprint_recognition.py
+++ src/fingerprint_recognition.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # _*_ coding: utf-8 _*_
 """Main module"""
+from numpy.core.multiarray import ndarray
 
 __author__ = "Nicola Onofri, Luigi Bonassi"
 __license__ = "GPL"
@@ -86,39 +87,31 @@ from utils import print_color_image
 
 
 def otsu(img: np.ndarray) -> np.ndarray:
+    """
+    Otsu binarization
+    :param img: the original image
+    :return: the binarized image
+    """
     blur = cv2.GaussianBlur(img, (5, 5), 0)
-    ret3, th3 = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    ret3, th3 = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
     return th3/255
 
 
 def ridge_thinning(img: np.ndarray) -> np.ndarray:
+    """
+    Ridge thinning or image skeletonization
+    :param img: the original image
+    :return: the skeleton of the image
+    """
     skeleton = skeletonize(img)
     skeleton = skeleton.astype(np.float)
     return skeleton
 
 
-# TODO display block orientation map
-def display_orientation_map(ridge_orientation: np.ndarray, block_size: int) -> np.ndarray:
-    """
-    Displays the vector field defined by the ridge orientation matrix
-    :param ridge_orientation: the ridge orientation matrix
-    :param block_size: the size of each block
-    :return:
-    """
-    result_shape = (ridge_orientation.shape[0]*block_size, ridge_orientation.shape[0]*block_size)
-    result_image = np.zeros(result_shape)
-
-    # loop over every element of the ridge orientation map
-
-    # convert the current angle to a line slope
-
-    # translate the line according to the block position
-    pass
-
-
-def gabor_filtering_block_level(img: np.ndarray, block_size: int = 16,
-                                gabor_kernel_size: Union[int, None] = None,
-                                lpf_size: int = 5, sobel_kernel_size: int = 5) -> Tuple[np.ndarray, np.ndarray]:
+def gabor_filtering(img: np.ndarray, block_size: int = 16,
+                    gabor_kernel_size: Union[int, None] = None,
+                    lpf_size: int = 5, sobel_kernel_size: int = 5,
+                    precise_orientation_map: bool = True) -> Tuple[np.ndarray, np.ndarray]:
     """
     Estimates the direction of each ridge and furrows using Hung least squares approximation
     and discard background blocks
@@ -130,14 +123,15 @@ def gabor_filtering_block_level(img: np.ndarray, block_size: int = 16,
                 By default it is set at 5.
     :param sobel_kernel_size: the size of the Sobel kernel used to extract partial derivatives.
                 By default it is set at 5.
+    :param precise_orientation_map: if True (default) uses a more precise algorithm for ridge direction estimation.
     :return: the filtered image
     """
     img_height, img_width = img.shape
     adapted_height, adapted_width = img_height, img_width
     filtered_image = np.zeros(img.shape, dtype=img.dtype)
-    # theta_map = {}
+    orientation_map = {}
     phi_map = {}
-    ridge_orientation_map = {}
+    improved_orientation_map = {}
 
     if gabor_kernel_size is None:
         gabor_kernel_size = block_size
@@ -161,27 +155,30 @@ def gabor_filtering_block_level(img: np.ndarray, block_size: int = 16,
 
             vx = np.sum(2*np.multiply(block_gx, block_gy))
             vy = np.sum(np.multiply(block_gx**2, block_gy**2))
-            theta_block = 0.5*np.arctan(np.divide(vy, vx+1e-6))  # scalar
-            # theta_map[(i//block_size, j//block_size)] = theta_block # leave it here, just in case
+            block_orientation = 0.5*np.arctan(np.divide(vy, vx+1e-6))  # scalar
+            orientation_map[(i, j)] = block_orientation
 
-            # smoothing & improving orientation approximation
-            phi_map[i, j] = (np.cos(2*theta_block), np.sin(2*theta_block))  # (phi_x, phi_y)
+            if precise_orientation_map:
+                # smoothing & improving orientation estimation
+                phi_map[i, j] = (np.cos(2*block_orientation), np.sin(2*block_orientation))  # (phi_x, phi_y)
 
-            # convert to continuous vector field, by expanding each phi_block
-            phi_x_block = phi_map[i, j][0]*np.ones(neighborhood_shape)
-            phi_y_block = phi_map[i, j][1]*np.ones(neighborhood_shape)
+                # convert to continuous vector field, by expanding each phi_block
+                phi_x_block = phi_map[i, j][0]*np.ones(neighborhood_shape)
+                phi_y_block = phi_map[i, j][1]*np.ones(neighborhood_shape)
 
-            # low pass filtering with a median kernel, unit integral
-            lpf_phi_x_block = cv2.blur(phi_x_block, (lpf_size, lpf_size))  # phi'_x
-            lpf_phi_y_block = cv2.blur(phi_y_block, (lpf_size, lpf_size))  # phi'_y
+                # low pass filtering with a median kernel, unit integral
+                lpf_phi_x_block = cv2.blur(phi_x_block, (lpf_size, lpf_size))  # phi'_x
+                lpf_phi_y_block = cv2.blur(phi_y_block, (lpf_size, lpf_size))  # phi'_y
 
-            # save the current orientation to the ridge map (dictionary)
-            current_block_ridge_orientation = 0.5*np.arctan(np.divide(lpf_phi_y_block, lpf_phi_x_block))[0, 0]
-            ridge_orientation_map[(i, j)] = current_block_ridge_orientation
-            print(current_block_ridge_orientation)
+                # save the current orientation to the ridge map (dictionary)
+                improved_block_orientation = 0.5*np.arctan(np.divide(lpf_phi_y_block, lpf_phi_x_block))[0, 0]
+                improved_orientation_map[(i, j)] = improved_block_orientation
+            else:
+                improved_block_orientation = block_orientation
+                improved_orientation_map = orientation_map
 
             # Gabor filtering according to estimated ridge block orientation
-            gabor_kernel = cv2.getGaborKernel((block_size, block_size), 1.0, current_block_ridge_orientation, 10.0, 1,
+            gabor_kernel = cv2.getGaborKernel((block_size, block_size), 1.0, improved_block_orientation, 10.0, 1,
                                               ktype=cv2.CV_32F)
             gabor_filtered_block = cv2.filter2D(image_block, -1, gabor_kernel). \
                 reshape(image_block.shape[0]*image_block.shape[1], )
@@ -192,12 +189,17 @@ def gabor_filtering_block_level(img: np.ndarray, block_size: int = 16,
                 filtered_image[current_coordinates[0], current_coordinates[1]] = px
 
     # reconvert direction_map to np.array
-    for coord, ridge_orientation in ridge_orientation_map.items():
+    for coord, ridge_orientation in improved_orientation_map.items():
         direction_map[coord[0], coord[1]] = ridge_orientation
     return filtered_image, direction_map
 
 
 def clean(img):
+    """
+    Perform the clean morphological operation
+    :param img: the original image
+    :return: the cleaned image
+    """
     height, width = img.shape
     new = np.zeros(img.shape, img.dtype)
     for i in range(height):
@@ -206,7 +208,7 @@ def clean(img):
             if new[i][j] == 1:
                 count = 0
                 for h in range(max(0, i-2), min(height, i+3)):
-                    for k in range(max(0, j - 2), min(width, j + 3)):
+                    for k in range(max(0, j-2), min(width, j+3)):
                         if img[h][k] == 1:
                             count += 1
                 if count <= 2:
@@ -215,17 +217,108 @@ def clean(img):
 
 
 def skeleton_enhancement(img):
+    """
+    Perform skeleton enhancement performing a sequence of morphological operations
+    :param img: the original skeletonized image
+    :return: an improved version of the skeletonized image
+    """
     tmp = clean(img)
     kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
     closing = cv2.morphologyEx(tmp, cv2.MORPH_CLOSE, kernel, iterations=1)
-    skel = ridge_thinning(closing)
-    return skel
+    enhanced_skeleton = ridge_thinning(closing)
+    return enhanced_skeleton
+
+
+# TODO display block orientation map
+def display_orientation_map(ridge_orientation: np.ndarray, block_size: int = 16) -> np.ndarray:
+    """
+    Displays the vector field defined by the ridge orientation matrix
+    :param ridge_orientation: the ridge orientation matrix
+    :param block_size: the size of each block. By default it is set to 16
+    :return:
+    """
+    result_shape = (ridge_orientation.shape[0]*block_size, ridge_orientation.shape[0]*block_size)
+    result_image = np.zeros(result_shape)
+
+    # loop over every element of the ridge orientation map
+
+    # convert the current angle to a line slope
+
+    # translate the line according to the block position
+    pass
+
+
+def find_lines(img):
+    """
+
+    :param img:
+    :return:
+    """
+    height, width = img.shape
+    label_map = {}
+    current_label = 1
+    for i in range(height):
+        for j in range(width):
+            if img[i][j] == 1 and (i, j) not in label_map:
+                label_map[(i, j)] = current_label
+                label_line(img, i, j, label_map, current_label, height, width)
+                current_label += 1
+    return label_map, current_label
+
+
+def label_line(img, i, j, label_map, label, height, width):
+    """
+
+    :param img:
+    :param i:
+    :param j:
+    :param label_map:
+    :param label:
+    :param height:
+    :param width:
+    :return:
+    """
+    for h in range(max(0, i-1), min(height, i+2)):
+        for k in range(max(0, j-1), min(width, j+2)):
+            if img[h][k] == 1 and (h, k) not in label_map:
+                label_map[(h, k)] = label
+                # recursive call
+                label_line(img, h, k, label_map, label, height, width)
+
+
+def print_fingerprint_lines(img, label_map, labels):
+    """
+    Displays identified furrows using different colors
+    :param img: the original image
+    :param label_map: the label dictionary
+    :param labels: the labels identified previously
+    :return:
+    """
+    height, width = img.shape
+    blank = np.zeros((height, width, 3), np.uint8)
+    current = 0
+    for l in range(1, labels+1):
+        pos = [k for k, v in label_map.items() if v == l]
+        if current != l:
+            current = l
+            r = np.random.randint(50, 255)
+            g = np.random.randint(50, 255)
+            b = np.random.randint(50, 255)
+        for (i, j) in pos:
+            blank[i][j] = (b, g, r)
+    print_color_image(blank)
 
 
 def pre_processing(img):
-    fingerprint = cv2.bitwise_not(img)
-    equalized = cv2.equalizeHist(fingerprint)
-    gabor_filtered, block_direction_map = gabor_filtering(img=equalized, block_size=16)
+    """
+    Preprocessing operations on the image
+    :param img: the original image
+    :return: the processed image
+    """
+    negated = cv2.bitwise_not(img)
+    equalized = cv2.equalizeHist(negated)
+    gabor_filtered, ridge_map = gabor_filtering(img=equalized, block_size=16,
+                                                precise_orientation_map=True)
     binarized = otsu(gabor_filtered)
     thinned = ridge_thinning(binarized)
     cleaned = clean(thinned)
@@ -233,52 +326,11 @@ def pre_processing(img):
     return enhanced
 
 
-def find_lines(img):
-    height, width = img.shape
-    label_map = {}
-    label = 1
-    for i in range(height):
-        for j in range(width):
-            if img[i][j] == 1 and (i, j) not in label_map:
-                label_map[(i, j)] = label
-                label_line(i, j, label_map, label, height, width)
-                label += 1
-    return label_map, label
-
-
-def label_line(i, j, label_map, label, height, width):
-    for h in range(max(0, i - 1), min(height, i + 2)):
-        for k in range(max(0, j - 1), min(width, j + 2)):
-            if img[h][k] == 1 and (h, k) not in label_map:
-                label_map[(h, k)] = label
-                label_line(h, k, label_map, label, height, width)
-
-
-def print_fingerprint_lines(label_map, label):
-    height, width = img.shape
-    blank = np.zeros((height, width, 3), np.uint8)
-    current = 0
-    for l in range(1, label+1):
-        pos = [k for k, v in label_map.items() if v == l]
-        if(current != l):
-            current = l
-            r = np.random.randint(50, 255)
-            g = np.random.randint(50, 255)
-            b = np.random.randint(50, 255)
-        for (i, j) in pos:
-         blank[i][j] = (b, g, r)
-    print_color_image(blank)
-
-
 if __name__ == '__main__':
-    fingerprint = load_image(filename="test4.jpg",
-                             cv2_read_param=0)
+    fingerprint = load_image(filename="orientation_map_test.jpg", cv2_read_param=0)
     # display_image(img=fingerprint, cmap="gray", title="Original fingerprint")
+    processed_img = pre_processing(fingerprint)
 
-    img = pre_processing(fingerprint)
-
-    label_map, label = find_lines(img)
-    print_fingerprint_lines(label_map, label)
-    gabor_filtered, ridge_map = gabor_filtering_block_level(img=equalized)
-    display_image(img=gabor_filtered, title="Gabor filtering")
-    print("End")
+    # furrows labeling
+    label_ridge_map, label_list = find_lines(processed_img)
+    print_fingerprint_lines(processed_img, label_ridge_map, label_list)
